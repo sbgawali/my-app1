2fc3227af5b9f3b395e46d32603acef45f472b1b
sbgawali <52544275+sbgawali@users.noreply.github.com>
2019-12-03 01:00:40 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import data from './appData.json'
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+import CourseList from './CourseList'
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={data:data}
+  }
+  render()
+  {
+    console.log({...[...data]})
+    return (
+      <React.Fragment>
+        <Container >
+        <CourseList  {...data}/>
+        </Container>
+     </React.Fragment>
+    );
+  }
 }
 
 export default App;
---
+++ src/SearchBar.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { logicalExpression } from '@babel/types';
+export default class CourseList extends Component{
+  constructor(){
+      super();
+      //this.state={data:data}
+  }
+  render(){
+return <h1>hiii</h1>
+  }
+
+
+}
